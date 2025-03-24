2cb76c640c3047ba01b33a9867a4c7cb9eb4d2b7
Fernando Salamanca <fernando.salamanca.barrera@gmail.com>
2025-03-24 10:11:24 -0500

    split and reuse interfaces

--- src/interfaces/main.ts
+++ src/interfaces/main.ts
@@ -1,29 +1,37 @@
-type NodeID = string
+type NodeID = string;
+
+type Sensor = {
+  category: string;
+  unit: string;
+};
+
+type IRealTimeSensorData = {
+  sensorId: string; // sensor_temperature_11
+  category: string; // temperature
+  value: number; // 22
+  alert: {
+    name: string; // NORMAL
+    color: string; // #C23DAA
+  };
+};
 
 export interface INodeIn3D {
-    id: NodeID
-    zone: {
-        type: string | "bocamina" | "tunel" | "extraction",
-        name: string
-    }
-    conections: NodeID[]
-    position: {
-        x: number,
-        y: number,
-        z: number
-    }
-    color: string | null
+  id: NodeID;
+  zone: {
+    category: string | "bocamina" | "tunel" | "extraction";
+    name: string;
+  };
+  conections: NodeID[];
+  position: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  color: string | null;
+  sensors: Sensor[];
 }
 
 export interface INodeRealTimeData {
-    id: NodeID
-    variables: {
-        type: string,
-        value: number,
-        unit: string,
-        alert: {
-            name: string
-            color: string
-        }
-    }[]
+  id: NodeID;
+  sensorsData: IRealTimeSensorData[];
 }
