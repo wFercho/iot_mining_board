e253d0258f3bd03fc9d3b03d32920e2145ea660d
Fernando Salamanca <fernando.salamanca.barrera@gmail.com>
2025-03-25 17:38:45 -0500

    fixup! feat: use context for darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { Route, Switch } from "wouter";
 import "./App.css";
-// import Screen from "./components/Screen";
+import Screen from "./components/Screen";
 import Mines from "./pages/Mines";
 import IoTGateway from "./pages/IoTGateway";
 import Sensors from "./pages/Sensors";
 import Nodes from "./pages/Nodes";
 import Home from "./pages/Home";
+import { AppProvider } from "./state/appContext";
 function App() {
   return (
-    <div className="h-full">
-      {/* <Screen mineId="ca90309e-8f17-49aa-80d9-e2ebc47e83f3" /> */}
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route path="/mines" component={Mines} />
-        <Route path="/iot-gateway" component={IoTGateway} />
-        <Route path="/nodes" component={Nodes} />
-        <Route path="/sensors" component={Sensors} />
+    <AppProvider>
+      <div className="h-full">
+        {/* <Screen mineId="ca90309e-8f17-49aa-80d9-e2ebc47e83f3" /> */}
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/mines" component={Mines} />
+          <Route path="/iot-gateway" component={IoTGateway} />
+          <Route path="/nodes" component={Nodes} />
+          <Route path="/sensors" component={Sensors} />
+          <Route path="/mine-nodes-3d/:mine_id" component={Screen} />
 
-        <Route path="/users/:name">
-          {(params) => <>Hello, {params.name}!</>}
-        </Route>
+          <Route path="/users/:name">
+            {(params) => <>Hello, {params.name}!</>}
+          </Route>
 
-        {/* Default route in a switch */}
-        <Route>404: No such page!</Route>
-      </Switch>
-    </div>
+          {/* Default route in a switch */}
+          <Route>404: No such page!</Route>
+        </Switch>
+      </div>
+    </AppProvider>
   );
 }
 
