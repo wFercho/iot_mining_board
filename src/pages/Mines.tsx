14a58c372e0a97bf31868c5b1798418414ae0f08
Fernando Salamanca <fernando.salamanca.barrera@gmail.com>
2025-04-04 20:24:33 -0500

    feat: use Mine registration page

--- src/pages/Mines.tsx
+++ src/pages/Mines.tsx
@@ -1,9 +1,17 @@
+import { Link } from "wouter";
 import { MinesTable } from "../components/MinesTable";
 import Dashboard from "../layouts/Dashboard";
 
 function Mines() {
   return (
     <Dashboard pageName="Minas">
+      <div className="flex justify-end mb-4">
+        <Link href="/mines/new">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded">
+            Nueva mina
+          </button>
+        </Link>
+      </div>
       <MinesTable />
     </Dashboard>
   );
